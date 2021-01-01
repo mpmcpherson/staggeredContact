c7d539016a3ea313c54b18693c4e3e3ddae6188e
Michael McPherson <michael@mpmcpherson.com>
2021-01-01 15:08:43 -0500

    Arranging some works

--- staggeredContacts/index.php
+++ staggeredContacts/index.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
-
 <html>
+<?php require 'resources/uiConfig.php';?>
 <head>
-<title>Add your title here</title>
+
+<title><?php echo $title; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 <link rel="stylesheet" href="css\style.css">
 
 </head>
 
 <body>
-	<?php require 'resources/logger.php';?>
+	
 
-	<div class="oneTenth pageHead">true header</div>
+	<div class="oneTenth pageHead"><h2><?php echo $header; ?></h2></div>
 
 	<div class="wrapper ninety" id="container">
 
 		<div class="leftBar ninety" id="leftBar">
-			left bar
+			<div style="font-weight: bold;"><?php echo $leftBar; ?></div>
+			<br />
+			<div id="array_of_people"></div>
 		</div>
 
 		<div class="mainBody ninety" id="body">
 			<div class="header">
-				<h3>Page Title</h3>
+				<h3><?php echo $viewportTitle; ?></h3>
 			</div> 
-			<!--<button id="btn">Top Button Text</button>-->
+			
 			<div id="text">Here's some text, and isn't it interesting?</div>
 			<div id="text">Lorem ipsum dolor sit amet. Dulche et decorum est</div>
 			<div id="text">Here's some text, and isn't it interesting?</div>
-			<div> End Text		</div>	
+			<div> End Text </div>	
 		</div>
 
 	</div>
@@ -36,30 +39,17 @@
 
 <script type="text/javascript" src="js\helper.js"></script>
 <script type="text/javascript">
-	var button = document.getElementById('btn');
-	var resultDiv = document.getElementById('results');
-
+	
 	docReady(function() {
-    	let leftbar = document.getElementById('leftBar');
-    	leftbar.setAttribute("class","leftBar full")
+    
 	});
 
  
-	button.addEventListener('click', function() {
-		//console.log("hit click");
-		alert("click");
-  		/*getRequest('basicGenerator.php',
-  			function(response){
-  				//console.log(response);
-  				resultDiv.innerHTML = response;
-  			},
-  			function(response){
-  				resultDiv.innerHTML = 'An error occurred during your request: ' +  response.status + ' ' + response.statusText;
-  			},
-  			null);
-*/
+	/*
+	variable.addEventListener('click', function() {
+	
 	});
-
+	*/
 
 
 </script>
---
+++ staggeredContacts/resources/uiConfig.php
@@ -0,0 +1,6 @@
+<?php
+	$header = "Let's make keeping in contact less awful";
+	$title = "Staggered Contacts";
+	$leftBar="People you want to keep in the loop";
+	$viewportTitle="Compose Message";
+?>
