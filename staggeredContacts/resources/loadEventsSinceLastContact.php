f8114b8c69a94da7bc5ca426b7c20abf3d848fa3
Michael McPherson <michael@mpmcpherson.com>
2021-01-02 10:22:35 -0500

    batch message

--- staggeredContacts/resources/loadEventsSinceLastContact.php
+++ staggeredContacts/resources/loadEventsSinceLastContact.php
@@ -3,17 +3,19 @@
 require 'defaultConnector.php';
 
 $userId = "";
+$personId="";
 
 $gets = json_decode(file_get_contents('php://input'), true);
 
 $userId = $gets["userId"];
+$personId = $gets["personId"];
 
 $qq = "select el.id, el.userId, el.EventListing from EventListing el
 	left join people p on p.possessingUserId = el.userId
 		left join lastContacted lc on lc.originelersonId = el.userId
 		left join userIntervalLink uil on uil.userId = lc.originPersonId
 		left join intervalTypes it on it.id = uil.intervalType
-		where el.possessingUserId = ".$userId." and p.id"."
+		where el.possessingUserId = ".$userId." and p.id'".$personId."
 		and CURRENT_DATE() >= DATE_ADD(lc.lastContactedDate, INTERVAL uil.intervalAmount it.intervalType)";
 
 $result = mysqli_query($dbhandle, $qq);
