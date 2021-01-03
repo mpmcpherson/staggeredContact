ecfc70d2466d504a13f6f0950a78ef21d00e66d8
Michael McPherson <michael@mpmcpherson.com>
2021-01-03 00:16:32 -0500

    batch message

--- staggeredContacts/resources/addPerson.php
+++ staggeredContacts/resources/addPerson.php
@@ -25,21 +25,22 @@ $intervalType = $gets["intervalType"];
 $channelName = $gets["channelName"];
 $channelValue = $gets["channelValue"];
 
-//var_dump($gets);
+$diagnosticOut = "";
 
 //person insert
 $qq = "insert into people(first_name, last_name, possessingUserId) values ('".$first_name."', '".$last_name."', ".$userId.")";
 $result = mysqli_query($dbhandle, $qq);
-simplog($result, "people insert successful\n");
+$diagnosticOut .= simplog($result, "people insert successful");
 
 //frequency insert
 $qq = "insert into userIntervalLink(userId, intervalAmount, intervalType) values (".$userId.", ".$intervalAmount.", ".$intervalType.")";
 $result = mysqli_query($dbhandle, $qq);
-simplog($result, "userIntervalLink insert successful\n");
+$diagnosticOut .= simplog($result, "userIntervalLink insert successful");
 
 $qq = "select id from people where people.first_name like '%".$first_name."%' and last_name like '%".$last_name."%' and possessingUserId =".$userId." limit 1";
 $result = mysqli_query($dbhandle, $qq);
-simplog($result, "peopleId select successful\n");
+$diagnosticOut .= simplog($result, "peopleId select successful");
+
 if($result){
 	$output = array();
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
@@ -54,16 +55,16 @@ if($result){
 //contact details
 $qq = "insert into contactChannels(personId, channelName, channelValue) values (".$personId.", '".$channelName."', '".$channelValue."')";
 $result = mysqli_query($dbhandle, $qq);
-simplog($result, "contacts insert successful\n");
+$diagnosticOut .= simplog($result, "contacts insert successful");
 
+echo json_encode($diagnosticOut);
 
 function simplog($result, $call){
 	if($result){
-		echo $call;
+		return $call."\n";
 	}else{
-		echo ("Could not insert data : " . mysqli_error($dbhandle) . " " . mysqli_errno($dbhandle));
+		return "Could not insert data : " . mysqli_error($dbhandle) . " " . mysqli_errno($dbhandle);
 	}
 }
-
 
 ?>
