f9f8e628b74a4362308c195fb48a06ea1d2da2b3
Michael McPherson <michael@mpmcpherson.com>
2021-01-31 19:18:43 -0500

    Helps if I use the goddamned verification function for verifying the password

--- staggeredContacts/resources/login.php
+++ staggeredContacts/resources/login.php
@@ -1,5 +1,5 @@
 <?php
-require('PasswordHash.php');
+
 require 'defaultConnector.php';
 
 $userName = "";
@@ -10,7 +10,7 @@ $gets = json_decode(file_get_contents('php://input'), true);
 $userName = $gets["userName"];
 $userPass = $gets["userPass"];
 
-$result = mysqli_query($dbhandle,"SELECT pw_hash, user_id from users where username like '$userName'");
+$result = mysqli_query($dbhandle,"SELECT pw_hash, user_id from general_dev_bastion.users where username like '$userName'");
 
 $outHash = 'firstassign';
 $userid = '';
@@ -18,16 +18,13 @@ $resPass = array();
 $userIdArr = array();
 
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    array_push($resPass, $outHash = $row['pw_hash']);
-    array_push($userIdArr, $userid = $row['user_id']);
+    array_push($resPass, $row['pw_hash']);
+    array_push($userIdArr, $row['user_id']);
 }
-$outHash = $resPass[0];
+
 $userid = $userIdArr[0];
 
-$hashing = password_hash($userPass, PASSWORD_DEFAULT);
-echo json_encode($hashing,$hashing);
-
-if (strcmp($hashing,$outHash)==0) {
+if (password_verify($userPass, $resPass[0])) {
     $rnd=rand();
     $_SESSION['valid'] = 1;
     $_SESSION['userid'] = $userid;
@@ -38,7 +35,7 @@ if (strcmp($hashing,$outHash)==0) {
     $newQ = "UPDATE general_dev_bastion.users SET sessionID = ".$rnd." WHERE user_id = '".$_SESSION['userid']."'";
     $result = mysqli_query($dbhandle, $newQ);
     
-    echo json_encode(true,$userid,$sessionID); 
+    echo json_encode([true,$userid,$sessionID]);
 } else { 
-    echo json_encode([$hashing, $res, null]); 
+    echo json_encode([$hashing, $resPass[0], null]);
 }
