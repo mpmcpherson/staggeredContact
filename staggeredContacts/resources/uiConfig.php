11ce8e7f785743d99f89ff4d83de492688811a6a
Michael McPherson <michael@mpmcpherson.com>
2021-02-14 21:28:48 -0500

    testing push

--- staggeredContacts/resources/uiConfig.php
+++ staggeredContacts/resources/uiConfig.php
@@ -1,5 +1,5 @@
 <?php
-	$header = "Let's make keeping in contact less awful, and test the deployment script";
+	$header = "Let's make keeping in contact less awful, and test the deployment script, and to make sufficient changes that the damn thing finally twigs";
 	$title = "Staggered Contacts";
 	$leftBar="People you want to keep in the loop";
 	$rightBar="What you've been up to since you last talked with them";
