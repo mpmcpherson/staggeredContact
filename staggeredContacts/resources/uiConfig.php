6cc45e83c820cd116073462515973843ef1333f8
Michael McPherson <michael@mpmcpherson.com>
2021-02-14 22:11:17 -0500

    autocommit Sun 14 Feb 2021 10:11:17 PM EST

--- staggeredContacts/resources/uiConfig.php
+++ staggeredContacts/resources/uiConfig.php
@@ -1,5 +1,5 @@
 <?php
-	$header = "Let's make keeping in contact less awful, and test the deployment script, and to make sufficient changes that the damn thing finally twigs";
+	$header = "Let's make keeping in contact less awful";
 	$title = "Staggered Contacts";
 	$leftBar="People you want to keep in the loop";
 	$rightBar="What you've been up to since you last talked with them";
