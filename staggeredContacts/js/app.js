e981c4d06d2fb5b99ff5248bb271d279ce44adb5
Michael McPherson <michael@mpmcpherson.com>
2022-03-15 00:46:40 -0400

    diagnostic

--- staggeredContacts/js/app.js
+++ staggeredContacts/js/app.js
@@ -244,7 +244,7 @@ function setPeopleEventListeners(){
 	const nodeItems = Array.from(peopleList);
 
 	for (let c of nodeItems) {
-			console.log("binding "+c.children[0].children[0].value); 
+			console.log("binding "+c.children); 
 				
 
 			c.addEventListener("click",function(event){
