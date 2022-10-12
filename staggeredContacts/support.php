77941df9d9de202ddeff3f772aca966b503a3130
Michael McPherson <michael@mpmcpherson.com>
2022-10-12 01:23:07 -0400

    alternate begging route

---
+++ staggeredContacts/support.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+	<?php require 'resources/uiConfig.php';?>
+	<head>
+		<title><?php echo $title; ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+		<link rel="stylesheet" href="css\style.css">
+		<link rel="stylesheet" href="css\modal.css">
+	</head>
+	<body onresize="fixHeight()">
+		<h1>Support</h1>
+		<p>So, I spent a fair whack of time building this, and I'm going to spend more maintaining it, and while I'm happy to do it, my landlord prefers that I spend my time making money to give to him, and my student loan company is almost as insistent. So, if you want to support me -and make it possible for me to keep doing this sort of thing with my time- it would be really cool if you threw a little cash my way. Here are a couple of ways you can do that!		
+
+	</body>
+</html>
+<script type="text/javascript">
+	function fixHeight(){}
+</script>
+<!--<script type="text/javascript" src="js\helper.js"></script>-->
+
+<script src="js\app.js" type="module"></script>
